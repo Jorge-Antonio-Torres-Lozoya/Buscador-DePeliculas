0d98985a6215e228f95173583656c12250483041
Jorge Torres <jtorres1@ucol.mx>
2022-10-12 09:36:50 -0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 // import { Router } from 'react-router-dom'
-import Router from './router'
+import Router from './Router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
