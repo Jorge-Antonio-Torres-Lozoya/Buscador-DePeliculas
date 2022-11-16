97820456dcd94a537c6f6b8e8b44b2dcdd67d6b9
Jorge Torres <jtorres1@ucol.mx>
2022-11-15 20:19:30 -0600

    first commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom'
 const Card = ({ name, id, image, rating }) => {
   return (
     <Link to={`/${id}`} className='text-decoration-none text-body'>
-      <div className='card'>
-        <img src={image && image.medium} alt='No existe imagen' className='card-img-top' />
-        <div className='card-body'>
+      <div className='card sizeCard'>
+        <img src={image && image.medium} alt='No existe imagen' className='card-img-top sizeImg' />
+        <div className='card-body text-center'>
           <h5 className='card-title'>{name}</h5>
         </div>
       </div>
